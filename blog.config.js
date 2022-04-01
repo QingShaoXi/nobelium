404326523615de122f0a041de7a9b61fad815743
Mo <m957915339@163.com>
2022-04-01 20:56:57 +0800

    Update blog.config.js
    #F0F8FF

--- blog.config.js
+++ blog.config.js
@@ -7,7 +7,7 @@ const BLOG = {
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
-  lightBackground: '#EBEBE4', // use hex value, don't forget '#' e.g #fffefc
+  lightBackground: '#F0F8FF', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#EBEBE4', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2021, // If leave this empty, current year will be used.
